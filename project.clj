598ae0f3527aaacd2f5e099c94294d3f080c48fc
Anton Kagakin <ask@chidori.ru>
2014-07-17 00:22:19 +0900

    fixed misprint

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webapp/lein-template "0.1.0"
+(defproject webapp/lein-template "0.1.1"
   :description "Simple webapp project template for leiningen"
   :url "https://github.com/chidori/webapp-template"
   :eval-in-leiningen true
--- src/leiningen/new/webapp/project.clj
+++ src/leiningen/new/webapp/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/your_user_name/{{name}}"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
-                 [httpkit "2.1.16"]
+                 [http-kit "2.1.16"]
                  [selmer "0.6.6"]]
   :aot :all)
